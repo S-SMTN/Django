09f568becf41aa60f2bd1c90c38c34d88a739498
S-SMTN <cven28@gmail.com>
2022-10-21 16:13:11 +0300

    studying

--- helloworld/initialization/urls.py
+++ helloworld/initialization/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('<slug:id>/search', initialization),
-    re_path(r'^old/(?P<year>[0-9]{4})/', archive),
+    path('', index, name='index'),
+    path('<slug:id>/search', initialization, name='search'),
+    re_path(r'^old/(?P<year>[0-9]{4})/', archive, name='archive'),
 ]
